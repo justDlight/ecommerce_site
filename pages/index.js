88b1320e913e662ec8af03cd9472b0a7ef254962
John <John.weiss1996@gmail.com>
2023-04-14 15:02:38 +1000

    Added catagories json file and connected to that to html as well as cases products

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,11 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 import products from '../products.json';
+import products2 from '../products2.json';
+import catagories from '../catagories.json';
 
 export default function Home() {
   return (
@@ -17,45 +20,121 @@ export default function Home() {
       </Head>
       <link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.0.21/default/snipcart.css" />
         
+
       
-
+      
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          justDlight&apos;s Computer Store!
-        </h1>
 
-        <p className={styles.description}>
-          <a className="snipcart-checkout snipcart-summary" href="#" style={{textDecoration: "none"}}>
-            <strong>Cart:</strong> <img src="https://lightintime.com/img/ecommerce_img/icons8-shopping-cart-64.png"></img><span className="snipcart-total-price">$0.00</span>
-          </a>
-        </p>
 
-        <h1 className={styles.title}>
-          New and Hot!
-        </h1>
-
-        <div className={styles.grid}>
-          {products.map(product => {
-            return (
-              <div key={product.id} className={styles.card}>
-                <img src={product.image} alt={`Preview of ${product.title}`} />
-                <h3>{ product.title }</h3>
-                <p>{ product.description }</p>
-                <p>${ product.price }</p>
-                <p>
-                  <button id="test_id" className="snipcart-add-item"
-                    data-item-id={product.id}
-                    data-item-image={product.image}
-                    data-item-name={product.title}
-                    data-item-url="/"
-                    data-item-price={product.price}
-                  >
-                    Add to Cart
-                  </button>
-                </p>
+        {/*Nav bar*/}
+        <div className="row" id="header-img">
+          <div className="row">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light" id="navbar">
+              <div className="container-fluid">
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                  <div className="navbar-nav">
+                    <a className="nav-link active" id="nav-link-2" aria-current="page" href="https://ecommerce-site-ten-henna.vercel.app/">Home</a>
+                    <a className="nav-link" id="nav-link-3" href="#">....</a>
+                    <a className="nav-link" id="nav-link-4" href="#">....</a>
+                    <p className="nav-link">
+                      <a className="snipcart-checkout snipcart-summary" href="#" style={{textDecoration: "none"}}>
+                        <img src="https://lightintime.com/img/ecommerce_img/icons8-shopping-cart-64.png"></img><span className="snipcart-total-price">$0.00</span>
+                      </a>
+                    </p>
+                  </div>
+                </div>
               </div>
-            );
-          })}
+            </nav>
+          </div>
+
+
+          {/*Title Homepage*/}
+          <div className="row">
+            <h1 className={styles.title} id="header-txt">
+              justDlight&apos;s Computer Store!
+            </h1>
+            <h1 id="sub-header-txt" className={styles.title}>
+              New and Hot!
+            </h1>
+          </div>
+
+          
+          {/*Catagories THIS IS NOT USED WILL BE DELETED*/}
+          <div className="row">
+            <div className="col" id="catagory-col">
+              <h4 id="catagories-title">Catagories</h4>
+              {catagories.map(category => (
+                <div key={category.title}>
+                  <h3>{category.title}</h3>
+                  <ul>
+                    {Object.keys(category['sub-cata']).map(subcat => (
+                      <li key={subcat}>{category['sub-cata'][subcat]}</li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+            </div>
+
+
+            {/*Products New and Hot*/}
+            <div className="col" id="product-col">
+              <div className={styles.grid}>
+                {products.map(product => {
+                  return (
+                    <div key={product.id} className={styles.card}>
+                      <img src={product.image} alt={`Preview of ${product.title}`} />
+                      <h3 id="product-title">{ product.title }</h3>
+                      <p id="product-description">{ product.description }</p>
+                      <p id="product-price">${ product.price }</p>
+                      <p>
+                        <button id="test_id" className="snipcart-add-item"
+                          data-item-id={product.id}
+                          data-item-image={product.image}
+                          data-item-name={product.title}
+                          data-item-url="/"
+                          data-item-price={product.price}
+                        >
+                          Add to Cart
+                        </button>
+                      </p>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+
+
+
+
+            {/*Products New and Hot*/}
+            <div className="col" id="product-col">
+              <div className={styles.grid}>
+                {products2.map(products2 => {
+                  return (
+                    <div key={products2.id} className={styles.card}>
+                      <img src={products2.image} alt={`Preview of ${products2.title}`} />
+                      <h3 id="product-title">{ products2.title }</h3>
+                      <p id="product-description">{ products2.description }</p>
+                      <p id="product-price">${ products2.price }</p>
+                      <p>
+                        <button id="test_id" className="snipcart-add-item"
+                          data-item-id={products2.id}
+                          data-item-image={products2.image}
+                          data-item-name={products2.title}
+                          data-item-url="/"
+                          data-item-price={products2.price}
+                        >
+                          Add to Cart
+                        </button>
+                      </p>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+
+
         </div>
       </main>
       <footer className={styles.footer}>
