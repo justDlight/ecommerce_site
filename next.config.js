bb8625e4f74ca94ec36d74844cd324b05070988d
John <John.weiss1996@gmail.com>
2023-04-18 18:05:13 +1000

    Vercel push trying to fix build error

--- next.config.js
+++ next.config.js
@@ -3,12 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-}
-
 module.exports = nextConfig
