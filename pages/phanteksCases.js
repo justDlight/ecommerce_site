360e4ca16dcc19545fa028998633fc3ae6a62bd8
John <John.weiss1996@gmail.com>
2023-04-18 15:06:07 +1000

    testing push to vercel

--- pages/phanteksCases.js
+++ pages/phanteksCases.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import 'bootstrap/dist/css/bootstrap.css'
+import { useEffect } from 'react';
 
 import cataHideTillClicked from '../jwCustomJS.js';
 import products from '../products.json';
